ff9775c540e1377418494d3ead1680d37b7a3b0e
Long H. Nguyen <lohnguyen47@gmail.com>
2021-05-06 08:49:50 -0700

    Add environments.yml

--- app/page.py
+++ app/page.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -11,12 +12,14 @@ def home(features):
 
     st.write("Here's our first attempt at using data to create a table:")
     st.write(df)
-
     st.write(features)
 
 
 def data_visualization():
     st.title('Data Visualization')
+
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+    st.line_chart(chart_data)
 
 
 def about_us():
